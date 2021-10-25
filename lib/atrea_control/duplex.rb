247781d1effbb8610a2b771d1b84eb76724e8133
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2021-10-25 22:53:26 +0200

    draft: use XML unit endpoint

--- lib/atrea_control/duplex.rb
+++ lib/atrea_control/duplex.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 require "logger"
+require "nokogiri"
+require "rest-client"
 require "selenium-webdriver"
 
 module AtreaControl
   # Controller for +control.atrea.eu+
   class Duplex
     CONTROL_URI = "https://control.atrea.eu/"
+
+    attr_reader :current_mode, :current_power, :outdoor_temperature
+    attr_reader :updated_at
 
     # @param [String] login
     # @param [String] password
@@ -65,43 +70,64 @@ module AtreaControl
     end
 
     def unit_id
-      return unless logged?
-
-      element = driver.find_element css: "div#pageTitle > a"
-      element.attribute(:href) =~ /unit=(\d+)/
-      Regexp.last_match(1)
+      user_unit["_unit"]
     end
 
-    # @return [Float]
-    def outdoor_temperature
-      return unless logged?
-
-      element = sensor_element(@sensors[__method__])
-      element.find_element(css: "div").text.to_f
-    end
-
-    # @return [Float] in %
-    def current_power
-      return unless logged?
-
-      # element = driver.find_element id: "contentBox#{@sensors[__method__]}"
-      element = sensor_element(@sensors[__method__])
-      element.find_element(css: "div:first-child").text.to_f
+    def user_id
+      user_unit["_user"]
     end
 
     # @return [String]
-    def current_mode
+    def user_auth
       return unless logged?
 
-      element = sensor_element(@sensors[__method__])
-      element.find_element(css: "div:first-child").text
+      @user_auth ||= driver.execute_script("return window.user")&.[] "auth"
     end
+
+    # @return [Hash]
+    def user_unit
+      return {} unless logged?
+      return @user_unit if @user_unit
+
+      element = driver.find_element(css: "#msgBox + script")
+      @user_unit = element.attribute(:innerHTML).strip.scan(/(\w+)='(\d+)'/).to_h
+    end
+
+    # # @return [Float]
+    # def outdoor_temperature
+    #   return unless logged?
+    #
+    #   element = sensor_element(@sensors[__method__])
+    #   element.find_element(css: "div").text.to_f
+    # end
+    #
+    # # @return [Float] in %
+    # def current_power
+    #   return unless logged?
+    #
+    #   # element = driver.find_element id: "contentBox#{@sensors[__method__]}"
+    #   element = sensor_element(@sensors[__method__])
+    #   element.find_element(css: "div:first-child").text.to_f
+    # end
+    #
+    # # @return [String]
+    # def current_mode
+    #   return unless logged?
+    #
+    #   element = sensor_element(@sensors[__method__])
+    #   element.find_element(css: "div:first-child").text
+    # end
 
     # quit selenium browser
     def close
       @logged = false
+      @user_unit = nil
+      @user_auth = nil
       driver.quit
+      remove_instance_variable @driver
     end
+
+    alias logout! close
 
     def as_json(_options = nil)
       {
@@ -117,11 +143,48 @@ module AtreaControl
     end
 
     def inspect
-      "<AtreaControl name: '#{name}' outdoor_temperature: #{outdoor_temperature}°C current_power: #{current_power}% current_mode: '#{current_mode}'>"
+      "<AtreaControl name: '#{name}' outdoor_temperature: #{outdoor_temperature}°C current_power: #{current_power}% current_mode: '#{current_mode}' updated_at=#{@updated_at}>"
     end
 
+    def call_unit!
+      return false unless logged?
+
+      params = {
+        _user: user_id,
+        _unit: unit_id,
+        auth: user_auth,
+        _t: "config/xml.xml",
+      }
+      response = RestClient.get "https://control.atrea.eu/comm/sw/unit.php", { Cookie: "autoLogin=#{CGI.escape([@login, @password].join("\b"))}", params: params }
+      @updated_at = Time.now
+      parse_response(response)
+    rescue RestClient::Forbidden
+      close
+      login && call_unit!
+    end
 
     private
+
+    def parse_response(response)
+      xml = Nokogiri::XML response.body
+      sensors_values = @sensors.transform_values do |id|
+        xml.xpath("//O[@I=\"#{id}\"]/@V").last&.value
+      end
+      refresh_data(sensors_values)
+    end
+
+    # @param [Hash] values
+    def refresh_data(values)
+      @outdoor_temperature = values[:outdoor_temperature].to_f / 10
+      @current_power = values[:current_power].to_f
+      @current_mode = mode_map[values[:current_mode]]
+
+      @sensors.keys.map { |key| [key, send(key)] }.to_h
+    end
+
+    def mode_map
+      { "0" => "Vypnuto", "1" => "Automat", "2" => "Větrání / CO2", "6" => "Rozvážení" }
+    end
 
     def logger
       @logger ||= ::Logger.new($stdout)
